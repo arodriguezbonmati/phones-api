22c52ae1c7fa808bb5ea2e5838c0bfc366cb6d63
Jaime Dordio <jaimedordio@gmail.com>
2020-03-24 11:24:59 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,9 +8,11 @@ const router = express.Router();
 app.use("/", router);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
   next();
 });
