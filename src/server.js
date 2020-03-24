1488a1b087687cc8b59051051a1448b9a652a0bb
Jaime Dordio <jaimedordio@gmail.com>
2020-03-24 11:20:10 +0100

    CORS

--- src/server.js
+++ src/server.js
@@ -6,6 +6,14 @@ const app = express();
 const router = express.Router();
 
 app.use("/", router);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 const port = process.env.PORT || 3000;
 
 router.get("/phones", (req, res) => {
@@ -14,6 +22,14 @@ router.get("/phones", (req, res) => {
 });
 
 app.listen(port);
-console.log(chalk.bgBlue("\n----------------------------------------------------------"));
-console.log(chalk.bgBlue("------------------ Running on port " + port + " ------------------"));
-console.log(chalk.bgBlue("----------------------------------------------------------\n"));
+console.log(
+  chalk.bgBlue("\n----------------------------------------------------------")
+);
+console.log(
+  chalk.bgBlue(
+    "------------------ Running on port " + port + " ------------------"
+  )
+);
+console.log(
+  chalk.bgBlue("----------------------------------------------------------\n")
+);
